21f4a39589d88b5c731c03bb008218ed76a49520
Hesham Elsayed <heesham749@gmail.com>
2025-06-11 01:38:18 +0300

    Enhance EmployeeRepository functionality and logging
    - Added detailed logging to Internallog.txt for NLog setup.
    - Updated EmployeeRepository to use new filtering, searching, and sorting methods.
    - Included System.Linq.Dynamic.Core package for dynamic LINQ queries.
    - Set default OrderBy value in EmployeeParameters for improved retrieval.
    - Extended RequestParameters with an OrderBy property for sorting.
    - Created RepositoryEmployeeExtensions for enhanced query capabilities.

---
+++ Repository/Extensions/RepositoryEmployeeExtensions.cs
@@ -0,0 +1,56 @@
+﻿using System.Linq.Dynamic.Core;
+using System.Reflection;
+using Entities.Models;
+
+namespace Repository.Extensions;
+
+public static class RepositoryEmployeeExtensions
+{
+    public static IQueryable<Employee> FilterEmployees(this IQueryable<Employee> employees, uint minAge, uint maxAge)
+        => employees.Where(e => e.Age >= minAge && e.Age <= maxAge);
+
+    public static IQueryable<Employee> Search(this IQueryable<Employee> employees, string? searchTerm)
+    {
+        if (string.IsNullOrWhiteSpace(searchTerm))
+            return employees;
+        return employees
+            .Where(e => e.Name!.Contains(searchTerm));
+    }
+
+    public static IQueryable<Employee> Sort(this IQueryable<Employee> employees, string? orderByQueryString)
+    {
+        if (string.IsNullOrWhiteSpace(orderByQueryString))
+            return employees.OrderBy(e => e.Name);
+
+        var orderParams = orderByQueryString.Split(',', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+
+        var propertyInfos = typeof(Employee).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+
+
+        var orderParts = new List<string>();
+
+        foreach (var param in orderParams)
+        {
+            var propertyNameFromQurey = param.Split(" ", StringSplitOptions.RemoveEmptyEntries)[0];
+
+            var objectProperty = propertyInfos.FirstOrDefault(pi
+                => pi.Name.Equals(propertyNameFromQurey, StringComparison.InvariantCultureIgnoreCase));
+
+            if (objectProperty is not null)
+            {
+                var direction = param.EndsWith(" desc") ? "descending" : "ascending";
+
+                orderParts.Add($"{objectProperty.Name} {direction}");
+            }
+        }
+
+        if (orderParts.Count == 0)
+            return employees.OrderBy(e => e.Name);
+
+        var orderQuery = string.Join(',', orderParts);
+
+        return employees.OrderBy(orderQuery);
+
+    }
+
+}
--- Shared/RequestFeatures/EmployeeParameters.cs
+++ Shared/RequestFeatures/EmployeeParameters.cs
@@ -2,9 +2,13 @@
 
 public class EmployeeParameters : RequestParameters
 {
+    public EmployeeParameters() => OrderBy = "name";
+
     public uint MinAge { get; set; }
 
     public uint MaxAge { get; set; } = int.MaxValue;
+
+    public string? SearchTerm { get; set; }
 
     public bool ValidAgeRange => MaxAge > MinAge;
 
