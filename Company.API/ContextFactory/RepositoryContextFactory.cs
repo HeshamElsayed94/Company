a73872927bf36cd7260d943765d51d8e9e7f13f6
Hesham Elsayed <heesham749@gmail.com>
2025-09-25 05:11:54 +0300

    u

--- Company.API/ContextFactory/RepositoryContextFactory.cs
+++ Company.API/ContextFactory/RepositoryContextFactory.cs
@@ -5,19 +5,20 @@ using Repository;
 namespace CompanyEmployees.API.ContextFactory;
 
 public class RepositoryContextFactory()
-    : IDesignTimeDbContextFactory<RepositoryContext>
+	: IDesignTimeDbContextFactory<RepositoryContext>
 {
-    public RepositoryContext CreateDbContext(string[] args)
-    {
-        var configuration = new ConfigurationBuilder()
-            .SetBasePath(Directory.GetCurrentDirectory())
-            .AddJsonFile("appsettings.json")
-            .Build();
 
-        var builder = new DbContextOptionsBuilder<RepositoryContext>()
-            .UseSqlServer(configuration.GetConnectionString("sqlConnection"),
-            b => b.MigrationsAssembly(typeof(Program).Assembly));
+	public RepositoryContext CreateDbContext(string[] args)
+	{
+		var configuration = new ConfigurationBuilder()
+			.SetBasePath(Directory.GetCurrentDirectory())
+			.AddJsonFile("appsettings.json")
+			.Build();
 
-        return new RepositoryContext(builder.Options);
-    }
+		var builder = new DbContextOptionsBuilder<RepositoryContext>()
+			.UseSqlServer(configuration.GetConnectionString("sqlConnection"),
+			b => b.MigrationsAssembly(typeof(Program).Assembly));
+
+		return new RepositoryContext(builder.Options);
+	}
 }
--- Company.Presentation/ApiBaseResponseExtensions/ApiBaseResponseExtensions.cs
+++ Company.Presentation/ApiBaseResponseExtensions/ApiBaseResponseExtensions.cs
@@ -4,6 +4,7 @@ namespace CompanyEmployees.Presentation.ApiBaseResponseExtensions;
 
 public static class ApiBaseResponseExtensions
 {
-    public static T GetResult<T>(this ApiBaseResponse apiBaseResponse)
-        => ((ApiOkResponse<T>)apiBaseResponse).Result;
+
+	public static T GetResult<T>(this ApiBaseResponse apiBaseResponse)
+		 => ((ApiOkResponse<T>)apiBaseResponse).Result;
 }
